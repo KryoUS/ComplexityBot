c5ab11d8df79a6a6cb3be0c4d5c088d2ced10bfe
Michael Hughes <taurussho89@gmail.com>
2018-06-10 11:00:55 -0600

    Added error embed for incorrect Role.

--- commands/quoteadd.js
+++ commands/quoteadd.js
@@ -13,12 +13,15 @@ module.exports = {
     cooldown: 60,
     async execute(message, args) {
         
-        if (message.member.roles.find("name", "RoleTest")) {
+        const quoteThumb = `https://firebasestorage.googleapis.com/v0/b/complexitywebsite-bdcf7.appspot.com/o/DiscordBot%2Fquotes.png?alt=media&token=357efdeb-b65d-4544-8e9d-14d66c5fc5b6`
+        const errorThumb = `https://firebasestorage.googleapis.com/v0/b/complexitywebsite-bdcf7.appspot.com/o/DiscordBot%2Ferror.png?alt=media&token=19beee91-6acd-4949-87da-dc2949e68fa1`
+
+        const allowedRoles = "RoleTest"
+
+        if (message.member.roles.find("name", allowedRoles)) {
 
             const quoteName = args[0]
             const firebaseAPI = `https://complexitywebsite-bdcf7.firebaseio.com/guildquotes.json?auth=${firebaseKey}`
-            const quoteThumb = `https://firebasestorage.googleapis.com/v0/b/complexitywebsite-bdcf7.appspot.com/o/DiscordBot%2Fquotes.png?alt=media&token=357efdeb-b65d-4544-8e9d-14d66c5fc5b6`
-            const errorThumb = `https://firebasestorage.googleapis.com/v0/b/complexitywebsite-bdcf7.appspot.com/o/DiscordBot%2Ferror.png?alt=media&token=19beee91-6acd-4949-87da-dc2949e68fa1`
             
             let argJoin = args.shift()
             argJoin = args.join(' ')
@@ -84,6 +87,22 @@ module.exports = {
 
                 message.channel.send({ embed: charEmbed });
             })
+        } else {
+            
+            const charEmbed = new Discord.RichEmbed()
+                .setColor('#ffffff')
+                //.setAuthor(`Quote Add`, weatherIcon)
+                .setTitle(`Quote Add: Permission Error`)
+                //.setURL(<url>)
+                .setDescription(`You do not have the required role to add a quote!`)
+                .setThumbnail(errorThumb)
+                .addField(`Allowed Roles`, allowedRoles, false)
+                //.addField(`Latitude and Longitude`, `Remember, North/South come first, followed by East/West. Both South and West are negative numbers. And finally, Latitude comes before Longitude.`, false)
+                //.setImage(<image>)
+                .setTimestamp()
+                .setFooter(`Requested by ${message.author.username}`, message.author.avatarURL);
+
+            message.reply({ embed: charEmbed });
         }
     },
 };
