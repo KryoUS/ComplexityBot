73e6b8ecc23807fa82d260ae7da84b05d5be45ae
KryoUS <taurussho89@gmail.com>
2021-04-14 20:53:54 -0600

    Fixed "undefined" bug with ternary for Romanji.

--- commands/anime.js
+++ commands/anime.js
@@ -50,7 +50,7 @@ module.exports = {
 
                     const charEmbed = new Discord.RichEmbed()
                         .setColor('#ffffff')
-                        .setTitle(`${anime.attributes.titles.en} [${anime.attributes.titles.ja_jp}]`)
+                        .setTitle(`${anime.attributes.titles.en ?? anime.attributes.titles.en_jp} [${anime.attributes.titles.ja_jp}]`)
                         .setURL(anime.links.self.replace('/api/edge', ''))
                         .setAuthor(`Anime`, botAvatar)
                         .setDescription(anime.attributes.description)
